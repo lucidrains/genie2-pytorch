bde730c985722c929a1ab67d323cd991cbe9904b
lucidrains <lucidrains@gmail.com>
2024-12-05 15:09:07 +0000

    divorce latent from model dimensions

--- genie2_pytorch/genie2.py
+++ genie2_pytorch/genie2.py
@@ -39,6 +39,7 @@ class Genie2(Module):
     def __init__(
         self,
         dim,
+        dim_latent,
         depth = 12,
         attn_dim_head = 64,
         heads = 8,
@@ -51,11 +52,14 @@ class Genie2(Module):
         self.encoder = encoder
         self.decoder = decoder
 
+        self.latent_to_model = nn.Linear(dim, dim_latent)
+        self.model_to_latent = nn.Linear(dim_latent, dim)
+
         self.transformer = Decoder(
             dim = dim,
             depth = depth,
             heads = heads,
-            attn_dim_head = dim_head,
+            attn_dim_head = attn_dim_head,
             **transformer_kwargs
         )
 
@@ -63,10 +67,12 @@ class Genie2(Module):
         self,
         state
     ):
-        encoded = self.encoder(state)
+        latents = self.encoder(state)
 
-        attended = self.transformer(encoded)
+        x = self.latent_to_model(latents)
+        x = self.transformer(x)
+        x = self.model_to_latent(x)
 
-        decoded = self.decoder(attended)
+        decoded = self.decoder(x)
 
         return decoded
