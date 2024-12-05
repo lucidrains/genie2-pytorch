d56fefb0547c8b60016dbe714471af92d018060f
lucidrains <lucidrains@gmail.com>
2024-12-05 16:09:43 +0000

    handle encoder / decoder accepting only images, einops makes everything easy

--- genie2_pytorch/genie2.py
+++ genie2_pytorch/genie2.py
@@ -56,18 +56,21 @@ class Genie2(Module):
         latent_channel_first = False,
         transformer_kwargs: dict = dict(),
         encoder: Module = nn.Identity(),
-        decoder: Module = nn.Identity()
+        decoder: Module = nn.Identity(),
+        is_video_enc_dec = False # by default will assume image encoder / decoder, but in the future, video diffusion models with temporal compression will likely perform even better, imo
     ):
         super().__init__()
 
         self.encoder = encoder
         self.decoder = decoder
 
+        self.is_video_enc_dec = is_video_enc_dec
+
         self.dim_latent = dim_latent
         self.latent_channel_first = latent_channel_first
 
-        self.latent_to_model = nn.Linear(dim, dim_latent)
-        self.model_to_latent = nn.Linear(dim_latent, dim)
+        self.latent_to_model = nn.Linear(dim_latent, dim)
+        self.model_to_latent = nn.Linear(dim, dim_latent)
 
         self.transformer = Decoder(
             dim = dim,
@@ -81,7 +84,17 @@ class Genie2(Module):
         self,
         state
     ):
+        need_fold_time_into_batch = self.is_video_enc_dec
+
+        if need_fold_time_into_batch:
+            state = rearrange(state, 'b c t ... -> b t c ...')
+            state, unpack_time = pack_one(state, '* c h w') # state packed into images
+
         latents = self.encoder(state)
+
+        if need_fold_time_into_batch:
+            latents = unpack_time(latents, '* c h w')
+            latents = rearrange(latents, 'b t c h w -> b c t h w')
 
         if self.latent_channel_first:
             latents = rearrange(latents, 'b d ... -> b ... d')
@@ -99,6 +112,28 @@ class Genie2(Module):
         if self.latent_channel_first:
             x = rearrange(x, 'b ... d -> b d ...')
 
+        if need_fold_time_into_batch:
+            x = rearrange(x, 'b c t h w -> b t c h w')
+            x, unpack_time = pack_one(x, '* c h w')
+
         decoded = self.decoder(x)
 
+        if need_fold_time_into_batch:
+            decoded = unpack_time(decoded, '* c h w')
+            decoded = rearrange(decoded, 'b t c h w -> b c t h w')
+
         return decoded
+
+# quick test
+
+if __name__ == '__main__':
+    genie = Genie2(
+        dim = 512,
+        dim_latent = 768,
+        latent_channel_first = True,
+        is_video_enc_dec = True
+    )
+
+    x = torch.randn(1, 768, 3, 2, 2)
+
+    out = genie(x)
